8927c4d9e5b26f9ca5b15d4628e4cffcadd0eefe
fatecsjm <Francisco.Guilhoto.T0105587@edu.atec.pt>
2017-11-14 18:31:58 +0000

    this commit is used to preserve the project before trying to install automapper -version:4.1

---
+++ Projeto/Controllers/Api/PaymentMethodController.cs
@@ -0,0 +1,82 @@
+﻿using Projeto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Projeto.Controllers.Api
+{
+    public class PaymentMethodController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+        public PaymentMethodController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/paymentmethod
+        public IEnumerable<PaymentMethod> GetPaymentMethods()
+        {
+
+            return _context.PaymentMethod.ToList();
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    _context.Dispose();
+        //}
+
+        // GET /api/paymentmethod/1
+        public PaymentMethod GetPaymentMethod(int id)
+        {
+            var payment = _context.PaymentMethod.SingleOrDefault(c => c.Id == id);
+
+            if (payment == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return payment;
+        }
+
+        // POST /api/paymentmethod
+        [HttpPost]
+        public PaymentMethod CreatePaymentMethod(PaymentMethod paymentMethod)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.PaymentMethod.Add(paymentMethod);
+            _context.SaveChanges();
+
+            return paymentMethod;
+        }
+        // PUT /api/paymentmethod/1
+        [HttpPut]
+        public void UpdatePaymentMethod(int id, PaymentMethod paymentMethod)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var paymentInDb = _context.PaymentMethod.SingleOrDefault(c => c.Id == id);
+            if (paymentInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            paymentInDb.Name = paymentMethod.Name;
+            _context.SaveChanges();
+
+        }
+
+        // Delete /api/paymentmethod/1
+        [HttpDelete]
+        public void DeletePaymentMethod(int id)
+        {
+            var paymentInDb = _context.PaymentMethod.SingleOrDefault(c => c.Id == id);
+            if (paymentInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _context.PaymentMethod.Remove(paymentInDb);
+            _context.SaveChanges();
+
+        }
+    }
+}
